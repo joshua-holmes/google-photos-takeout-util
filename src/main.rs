65f4a1bcd6494c13464af09b00c0fe5218e9bf1c
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2025-04-16 21:38:42 -0700

    Get multithreaded file picker to work

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello, world!");
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+#![allow(rustdoc::missing_crate_level_docs)] // it's an example
+
+use std::{path::PathBuf, sync::mpsc, thread::JoinHandle};
+
+use eframe::egui;
+
+mod views;
+
+fn main() -> eframe::Result {
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([640.0, 240.0])
+            .with_drag_and_drop(true),
+        ..Default::default()
+    };
+
+    // Our application state:
+    eframe::run_native(
+        "Native file dialogs and drag-and-drop files",
+        options,
+        Box::new(|_cc| Ok(Box::<MyApp>::default())),
+    )
 }
+
+#[derive(Default)]
+struct MyApp {
+    dropped_files: Vec<egui::DroppedFile>,
+    picked_path: Option<PathBuf>,
+    rx: Option<mpsc::Receiver<PathBuf>>,
+    handle: Option<JoinHandle<()>>,
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            views::file_picker::show(self, ctx, ui);
+        });
+    }
+}
+
---
+++ src/views/file_picker.rs
@@ -0,0 +1,109 @@
+use eframe::egui;
+use std::time::Duration;
+
+use crate::MyApp;
+
+pub fn show(app: &mut MyApp, ctx: &egui::Context, ui: &mut egui::Ui) {
+    ui.vertical_centered_justified(|ui| {
+        ui.label("Drag-and-drop files onto the window!");
+        if ui.button("Open file…").clicked() {
+            let (tx, rx) = std::sync::mpsc::channel();
+            let handle = std::thread::spawn(move || {
+                if let Some(path) = rfd::FileDialog::new().pick_file() {
+                    match tx.send(path) {
+                        Ok(_) => {}
+                        Err(err) => println!("Uh oh {:?}", err.to_string()),
+                    }
+                }
+            });
+            app.rx = Some(rx);
+            app.handle = Some(handle);
+        }
+
+        if let Some(picked_path) = app.rx.as_ref().and_then(|rx| rx.recv_timeout(Duration::new(0, 1_000_000)).ok()) {
+            app.picked_path = Some(picked_path);
+            app.rx = None;
+            app.handle.take().unwrap().join().unwrap();
+        }
+
+        if let Some(picked_path) = app.picked_path.as_ref() {
+            ui.horizontal(|ui| {
+                ui.label("Picked file:");
+                ui.monospace(picked_path.display().to_string());
+            });
+        }
+
+        // Show dropped files (if any):
+        if !app.dropped_files.is_empty() {
+            ui.group(|ui| {
+                ui.label("Dropped files:");
+
+                for file in &app.dropped_files {
+                    let mut info = if let Some(path) = &file.path {
+                        path.display().to_string()
+                    } else if !file.name.is_empty() {
+                        file.name.clone()
+                    } else {
+                        "???".to_owned()
+                    };
+
+                    let mut additional_info = vec![];
+                    if !file.mime.is_empty() {
+                        additional_info.push(format!("type: {}", file.mime));
+                    }
+                    if let Some(bytes) = &file.bytes {
+                        additional_info.push(format!("{} bytes", bytes.len()));
+                    }
+                    if !additional_info.is_empty() {
+                        info += &format!(" ({})", additional_info.join(", "));
+                    }
+
+                    ui.label(info);
+                }
+            });
+        }
+    });
+
+    preview_files_being_dropped(ctx);
+
+    // Collect dropped files:
+    ctx.input(|i| {
+        if !i.raw.dropped_files.is_empty() {
+            app.dropped_files.clone_from(&i.raw.dropped_files);
+        }
+    });
+}
+
+fn preview_files_being_dropped(ctx: &egui::Context) {
+    use egui::{Align2, Color32, Id, LayerId, Order, TextStyle};
+    use std::fmt::Write as _;
+
+    if !ctx.input(|i| i.raw.hovered_files.is_empty()) {
+        let text = ctx.input(|i| {
+            let mut text = "Dropping files:\n".to_owned();
+            for file in &i.raw.hovered_files {
+                if let Some(path) = &file.path {
+                    write!(text, "\n{}", path.display()).ok();
+                } else if !file.mime.is_empty() {
+                    write!(text, "\n{}", file.mime).ok();
+                } else {
+                    text += "\n???";
+                }
+            }
+            text
+        });
+
+        let painter =
+            ctx.layer_painter(LayerId::new(Order::Foreground, Id::new("file_drop_target")));
+
+        let screen_rect = ctx.screen_rect();
+        painter.rect_filled(screen_rect, 0.0, Color32::from_black_alpha(192));
+        painter.text(
+            screen_rect.center(),
+            Align2::CENTER_CENTER,
+            text,
+            TextStyle::Heading.resolve(&ctx.style()),
+            Color32::WHITE,
+        );
+    }
+}
