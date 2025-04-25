b816ee3f1f4e2c220cca8a32fd25912858753a2b
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2025-04-25 16:45:59 -0700

    Change unzip to not return list of files and change recursive function to return hashset not vec

--- src/services/utils.rs
+++ src/services/utils.rs
@@ -1,11 +1,11 @@
-use std::{collections::VecDeque, fs, io, path::Path};
+use std::{collections::{HashSet, VecDeque}, fs, io, path::Path};
 
-pub fn unzip(zip_path: &Path) -> Vec<std::path::PathBuf> {
+/// Unzips given zip file, creating a new directory for unzipped contents and only keeping files (not empty directories).
+pub fn unzip(zip_path: &Path) -> std::path::PathBuf {
     let file = fs::File::open(zip_path).unwrap();
     let mut archive = zip::ZipArchive::new(file).unwrap();
     let working_dir = zip_path.parent().unwrap().join(zip_path.file_stem().unwrap());
     fs::create_dir_all(&working_dir).unwrap();
-    let mut outpaths = Vec::with_capacity(archive.len());
     for i in 0..archive.len() {
         let mut file = archive.by_index(i).unwrap();
         if file.is_dir() {
@@ -39,20 +39,19 @@ pub fn unzip(zip_path: &Path) -> Vec<std::path::PathBuf> {
                 fs::set_permissions(&outpath, fs::Permissions::from_mode(mode)).unwrap();
             }
         }
-        outpaths.push(outpath);
     }
-    outpaths
+    working_dir
 }
 
-/// Recursively read a given directory and return vector of all file names.
-pub fn recursively_collect_filenames(path: &Path) -> std::io::Result<Vec<std::path::PathBuf>> {
-    let mut paths = Vec::new();
+/// Recursively read a given directory and return hash set of all file names.
+pub fn recursively_collect_filenames(path: &Path) -> std::io::Result<HashSet<std::path::PathBuf>> {
+    let mut paths = HashSet::new();
     let mut queue = VecDeque::from([path.to_owned()]);
     while let Some(path) = queue.pop_back() {
         for maybe_dir in fs::read_dir(&path)? {
             let dir = maybe_dir?;
             if dir.path().is_file() {
-                paths.push(dir.path());
+                paths.insert(dir.path());
             } else if dir.path().is_dir() {
                 queue.push_front(dir.path());
             }
@@ -65,7 +64,7 @@ pub fn recursively_collect_filenames(path: &Path) -> std::io::Result<Vec<std::pa
 
 #[cfg(test)]
 mod tests {
-    use std::{collections::HashSet, path::PathBuf};
+    use std::path::PathBuf;
     use super::*;
 
     #[test]
@@ -86,58 +85,17 @@ mod tests {
     }
 
     #[test]
-    fn collect_filenames_returns_same_results_as_unzip() {
+    fn collect_filenames_returns_correct_result_after_unzip() {
         // copy zip for use in tests
         let original = "./test-assets/takeout.zip";
         let test_dir = "./test-assets/collect_filenames_returns_same_results_as_unzip";
         let test_zip = test_dir.to_string() + ".zip";
         fs::copy(original, &test_zip).unwrap();
 
-        let unzip_paths = unzip(Path::new(&test_zip));
-        let paths = recursively_collect_filenames(Path::new("./test-assets/takeout-unzipped/")).unwrap();
+        let unzip_path = unzip(Path::new(&test_zip));
+        let paths = recursively_collect_filenames(&unzip_path).unwrap();
 
-        // ensure both return same number of files
-        assert_eq!(unzip_paths.len(), paths.len());
-
-        // put just the file names in hash sets
-        let mut unziped_filenames = HashSet::new();
-        let mut filenames = HashSet::new();
-        for p in unzip_paths.iter() {
-            unziped_filenames.insert(p.file_name().unwrap());
-        }
-        for p in paths.iter() {
-            filenames.insert(p.file_name().unwrap());
-        }
-
-        // compare hash sets
-        for p in unziped_filenames.iter() {
-            println!("Ensuring {:?}, which was unzipped, is in other directory.", p.display());
-            assert!(filenames.contains(p));
-        }
-        for p in filenames.iter() {
-            println!("Ensuring {:?}, which is in regular directory, is found in unzipped directory.", p.display());
-            assert!(unziped_filenames.contains(p));
-        }
-
-
-        // cleanup
-        fs::remove_dir_all(test_dir).unwrap();
-        fs::remove_file(test_zip).unwrap();
-    }
-
-    #[test]
-    fn unzip_returns_correct_number_of_files() {
-        // copy zip for use in tests
-        let original = "./test-assets/takeout.zip";
-        let test_dir = "./test-assets/unzip_returns_correct_number_of_files";
-        let test_zip = test_dir.to_string() + ".zip";
-        fs::copy(original, &test_zip).unwrap();
-
-        // unzip
-        let outpaths = unzip(Path::new(&test_zip));
-
-        // assert
-        assert_eq!(outpaths.len(), 8);
+        assert_eq!(paths.len(), 8);
 
         // cleanup
         fs::remove_dir_all(test_dir).unwrap();
